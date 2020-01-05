ef5fb0f26536dae6cb0c3b98bf7c0d7753efe136
Dev3 <dev3@shayansolutions.com>
2020-01-05 21:19:55 +0500

    Fixes

--- app/src/main/java/com/example/milkdeliveryapp/MainActivity.kt
+++ app/src/main/java/com/example/milkdeliveryapp/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.milkdeliveryapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this@MainActivity, "Helo", Toast.LENGTH_LONG).show();
     }
 }
